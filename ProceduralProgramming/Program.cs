e356a96e7a48c080ff90fbec2df53087f3e6c6ca
JohnMorris4 <johnmorris@morrisje.com>
2018-03-30 05:00:37 -0400

    Setup for new section

--- ProceduralProgramming/Program.cs
+++ ProceduralProgramming/Program.cs
@@ -10,6 +10,21 @@ namespace ProceduralProgramming
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your name? ");
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+            Console.WriteLine("Reversed Name: " + reversed);
+
+        }
+
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - 1] = name[i - 1];
+
+            return new string(array);
+            
         }
     }
 }
